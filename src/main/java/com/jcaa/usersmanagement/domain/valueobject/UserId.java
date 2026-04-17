30af6449c5f4b67686eb6aa1ea15bac1a04d488b
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-04-17 13:40:36 -0500

    refactor: se corrigio la violacion a la regla regla 4 en la clase UserId

--- src/main/java/com/jcaa/usersmanagement/domain/valueobject/UserId.java
+++ src/main/java/com/jcaa/usersmanagement/domain/valueobject/UserId.java
@@ -1,18 +1,19 @@
 package com.jcaa.usersmanagement.domain.valueobject;
 
+import java.util.Objects;
+
 import com.jcaa.usersmanagement.domain.exception.InvalidUserIdException;
 
 public record UserId(String value) {
 
   public UserId {
-    // VIOLACIÓN Regla 4: se usa == null en lugar de Objects.requireNonNull() o Objects.isNull().
-    // Para objetos siempre debe usarse Objects.isNull/nonNull, nunca operadores == o !=.
-    if (value == null) {
+
+    if (Objects.isNull(value)) {
       throw new NullPointerException("UserId cannot be null");
     }
     final String normalizedValue = value.trim();
     validateNotEmpty(normalizedValue);
-    // asigna el valor normalizado al componente
+
     value = normalizedValue;
   }
 
