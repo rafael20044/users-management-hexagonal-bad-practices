cbfb1bda39fc35fb387aa7efd35d94867d5873a0
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-04-17 12:34:51 -0500

    refactor: se corrigio la violacion a la regla regla 10 en la clase UserNotFoundException

--- src/main/java/com/jcaa/usersmanagement/domain/exception/UserNotFoundException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/UserNotFoundException.java
@@ -2,13 +2,13 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class UserNotFoundException extends DomainException {
 
+  private static final String MESSAGE_NOT_FOUND = "The user with id '%s' was not found.";
+
   private UserNotFoundException(final String message) {
     super(message);
   }
 
   public static UserNotFoundException becauseIdWasNotFound(final String userId) {
-    // VIOLACIÓN Regla 10: texto de error hardcodeado directamente en el método fábrica.
-    // Debe usarse una constante con nombre descriptivo en lugar de un String literal.
-    return new UserNotFoundException(String.format("The user with id '%s' was not found.", userId));
+    return new UserNotFoundException(String.format(MESSAGE_NOT_FOUND, userId));
   }
 }
