5d4cbafa42e61e98f4f34cf219065b4773ad2662
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-04-17 13:53:39 -0500

    refactor: se corrigio la violacion a la regla regla 4 en la clase UserPassword

--- src/main/java/com/jcaa/usersmanagement/domain/valueobject/UserPassword.java
+++ src/main/java/com/jcaa/usersmanagement/domain/valueobject/UserPassword.java
@@ -20,8 +20,7 @@ public final class UserPassword {
    * crea o cambia su contraseña.
    */
   public static UserPassword fromPlainText(final String plainText) {
-    // VIOLACIÓN Regla 4: se usa == null en lugar de Objects.isNull() o Objects.requireNonNull()
-    if (plainText == null) {
+    if (Objects.isNull(plainText)) {
       throw new NullPointerException("Password cannot be null");
     }
     final String normalizedValue = plainText.trim();
