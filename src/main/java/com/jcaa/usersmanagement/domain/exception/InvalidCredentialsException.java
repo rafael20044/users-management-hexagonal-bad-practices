e26174d172080342c1fa1a008540fdfc8e33a9df
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-04-17 11:56:46 -0500

    refactor: se corrigio la violacion a la regla regla 10 en la clase IvalidCredentialsException

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidCredentialsException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidCredentialsException.java
@@ -2,14 +2,14 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class InvalidCredentialsException extends DomainException {
 
+  private static final String MESSAGE_INVALID_CREDENTIALS = "Correo o contraseña incorrectos.";
+
   private InvalidCredentialsException(final String message) {
     super(message);
   }
 
   public static InvalidCredentialsException becauseCredentialsAreInvalid() {
-    // VIOLACIÓN Regla 10: texto de error hardcodeado directamente.
-    // Debe usarse una constante con nombre descriptivo en lugar de un String literal.
-    return new InvalidCredentialsException("Correo o contraseña incorrectos.");
+    return new InvalidCredentialsException(MESSAGE_INVALID_CREDENTIALS);
   }
 
   public static InvalidCredentialsException becauseUserIsNotActive() {
