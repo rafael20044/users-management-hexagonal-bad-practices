04d1e83a857619899decc2295c126f783ef7dd0c
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-04-17 12:27:28 -0500

    refactor: se corrigio la violacion a la regla regla 10 en la clase IvalidUserRoleException

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserRoleException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserRoleException.java
@@ -2,12 +2,13 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class InvalidUserRoleException extends DomainException {
 
+  private static final String MESSAGE_IS_INVALID = "The user role '%s' is not valid.";
+
   private InvalidUserRoleException(final String message) {
     super(message);
   }
 
   public static InvalidUserRoleException becauseValueIsInvalid(final String role) {
-    // VIOLACIÓN Regla 10: texto hardcodeado directamente — debe ser una constante.
-    return new InvalidUserRoleException(String.format("The user role '%s' is not valid.", role));
+    return new InvalidUserRoleException(String.format(MESSAGE_IS_INVALID, role));
   }
 }
