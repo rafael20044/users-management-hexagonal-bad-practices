04d99fa7388bcf141752d28fc82a0dd53143b9a1
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2026-04-17 17:49:49 -0500

    refactor: se corrigio la violacion a la regla regla 4

--- src/main/java/com/jcaa/usersmanagement/infrastructure/config/ValidatorProvider.java
+++ src/main/java/com/jcaa/usersmanagement/infrastructure/config/ValidatorProvider.java
@@ -3,10 +3,11 @@ package com.jcaa.usersmanagement.infrastructure.config;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import lombok.experimental.UtilityClass;
+
 import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 
-// VIOLACIÓN Regla 4: clase con solo métodos estáticos que NO tiene @UtilityClass ni constructor privado.
-// Debería anotarse con @UtilityClass para evitar instanciación accidental y generar el constructor privado automáticamente.
+@UtilityClass
 public final class ValidatorProvider {
 
 
